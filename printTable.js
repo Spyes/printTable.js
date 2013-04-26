69a84c5dcdfcd0ff5fac15f439f21bd8bf3760f0
Lewis Nitzberg <lewis@lucifer.home>
2013-04-26 17:00:56 +0300

    Major changes and fixes

--- printTable.js
+++ printTable.js
@@ -1,55 +1,95 @@
 /** Dependencies: JQuery, underscore (or lodash) **/
 
-function printTable(splitClass) {
-  var about = {
-    Version: 0.1,
-    Author: "Lewis Nitzberg",
-    Created: "April 2013",
-    Updated: "25 Apr 2013"
-  },
-    max_page_height = 550,  // change to get dynamically
-    splits,  // how many times to split the table
-    table_height,
-    tr_sum,
-    $t,
-    $cbody,
-    $tr,
-    $br,
-    copy;
+(function($) {
+  var defaults = { split_class: "splitTable",
+                   print_class: "printTable",
+                   print: true,
+                   repeat_header: true,
+                   max_page_height: 550 },
+    settings = {};
 
-  if (splitClass) {
-    if (typeof splitClass === "string") {
-      table_height = $('table.' + splitClass).height();
+  function Iframe() {
+    var iframeStyle = 'border:0;position:absolute;width:0px;height:0px;left:0px;top:0px',
+      iframe;
 
-      if (table_height > max_page_height) {
-        splits = Math.ceil(table_height / max_page_height);
+    try {
+      iframe = document.createElement('iframe');
+      document.body.appendChild(iframe);
+      $(iframe).attr({style: iframeStyle});
+      iframe.doc = null;
+      iframe.doc = iframe.contentDocument ? iframe.contentDocument : (iframe.contentWindow ? iframe.contentWindow.document : iframe.document);
+    } catch (e) {
+      throw e + ". iframes may not be supported in this browser.";
+    }
 
-        _.times(splits - 1, function () {
-          tr_sum = 0;
-          $t = $('table.' + splitClass);
+    if (iframe.doc === null) {
+      throw "Cannot find document.";
+    }
 
-          // clone the original table
-          copy = $t.clone();
-          copy.find('tbody > tr').remove();
-          $cbody = copy.find('tbody');
-          $t.removeClass(splitClass);
-          $('tbody tr', $t).each(function () {
-            $tr = $(this);
-            tr_sum = tr_sum + $tr.height();
-            if (tr_sum >= max_page_height) {
-              $tr.detach().appendTo($cbody);
+    return iframe;
+  }
+
+  $.fn.printTable = function (options) {
+    var about = {
+      Version: 0.2,
+      Author: "Lewis Nitzberg",
+      Created: "April 2013",
+      Updated: "25 Apr 2013"
+    },
+      table_height,
+      tr_sum,
+      table_index,
+      $t,
+      $cbody,
+      $chead,
+      $tr,
+      $br,
+      f,
+      writeDoc,
+      printWindow,
+      copy;
+
+    $.extend(settings, defaults, options);
+    f = new Iframe();
+    writeDoc = f.doc;
+    printWindow = f.contentWindow || f;
+
+    _.each($(this), function (table) {
+      $t = $(table);
+      table_height = $t.height();
+      table_index = 0;
+      if (table_height > settings.max_page_height) {
+        tr_sum = 0;
+        copy = $t.clone();
+        copy.find('tbody > tr').remove();
+        table_height = $t.height();
+        $cbody = copy.find('tbody');
+        $('tbody tr', $t).each(function () {
+          $tr = $(this);
+          if ((tr_sum + $tr.height()) > settings.max_page_height) {
+            tr_sum = 0;
+
+            // move old table to iframe
+            if (table_index > 0) {
+              copy.css('page-break-before', 'always');
             }
-          });
+            copy.appendTo(writeDoc.body);
+            table_index += 1;
 
-          $br = $('<div></div>')
-            .css('page-break-before', 'always');
-          $br.insertAfter($t);
-          $t.addClass('printTable');
-          copy.insertAfter($br);
+            // create a new table
+            copy = $t.clone();
+            copy.find('tbody > tr').remove();
+            $cbody = copy.find('tbody');
+          } else {
+            tr_sum = tr_sum + $tr.height();
+            $tr.clone().appendTo($cbody);
+            $cbody.appendTo(copy);
+          }
         });
       }
-    }
-  } else {
-    return about;
+    });
+
+    printWindow.focus();
+    printWindow.print();
   }
-}
+})(jQuery);
